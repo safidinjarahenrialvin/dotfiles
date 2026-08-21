96c7b493badaea1d0bb11f0cf1d5afa8bb963f11
Henri Alvin <safidinjarahenrialvin@gmail.com>
2026-08-21 17:42:49 +0300

    modif waybar

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -253,24 +253,8 @@ hl.config({
 
 hl.gesture({
 	fingers = 3,
-	direction = "vertical",
+	direction = "horizontal",
 	action = "workspace",
-})
-
-hl.gesture({
-	fingers = 3,
-	direction = "left",
-	action = function()
-		hl.dispatch(hl.dsp.layout("focus r"))
-	end,
-})
-
-hl.gesture({
-	fingers = 3,
-	direction = "right",
-	action = function()
-		hl.dispatch(hl.dsp.layout("focus l"))
-	end,
 })
 
 -- Example per-device config
